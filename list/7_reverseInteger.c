5e5699ed22ee34b815eaec157b6d0c58d5f2c325
cj187127689 <1763539293@qq.com>
2019-11-10 00:03:28 +0800

    update

--- list/7_reverseInteger.c
+++ list/7_reverseInteger.c
@@ -17,6 +17,16 @@
 			负值最大 INT_MIN = -2147483648
 			正值最大 INT_MAX = 2147483647
 */
+/*更新一个评论区的大神代码*/
+int reverse(int x)
+{
+    int max = 0x7fffffff, min = 0x80000000;//int的最大值最小值
+    long rs = 0;//用long类型判断溢出
+    for(;x;x/=10){//逆序，正负通吃，不用单独考虑负值{
+		rs = rs*10+x%10;
+	}
+    return rs>max||rs<min?0:rs;//超了最大值低于最小值就返回0
+}
 
 int reverse(int x)
 {
